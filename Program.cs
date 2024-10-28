2c7efc702d6ad5001a814d6cc21d971b5a5eeb4a
wam0824 <wam082024@gmail.com>
2024-10-28 13:43:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FreeCoder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       string firstName;
+        firstName = "Bob";
+
+        int number;
+        number = 3;
+
+        float temperature;
+        temperature = 34.4F;
+
+        Console.WriteLine($"Hello, {firstName}! You have {number} messages in your inbox. The temperature is {temperature} celsius.");
+    }
+}
